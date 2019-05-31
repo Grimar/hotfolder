5dceca69a438a64351c807599aca121e2a1239d9
Grimar <victor310395@gmail.com>
2019-05-31 22:04:11 +0300

    started switch to pyqt5

--- client.py
+++ client.py
@@ -1,13 +1,18 @@
-from PyQt4 import QtGui
+from PyQt5 import QtWidgets, QtGui
 import sys
 def main():
-    app = QtGui.QApplication(sys.argv)
-    w = QtGui.QWidget()
-    table = QtGui.QTableWidget(5,5,w)
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    table = QtWidgets.QTableWidget(5,5,w)
     table.move(0,0)
+    w.setWindowTitle("Hotfolder client 0.1")
+    w.setWindowIcon(QtGui.QIcon("icon.png"))
+    w.setMinimumSize(400,300)
+    table.resize(w.size())
 
     w.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 if __name__ == '__main__':
     main()
+
