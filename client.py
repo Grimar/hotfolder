3d136ddef0e779157b90f84152768ce45a111d15
victor <victor310395@gmail.com>
2019-06-04 16:53:44 +0300

    dialog file selection and store

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import sys
 import hotfolder
 from ui import mainwindow, folderDialog
 import os.path
+import shelve
 
 
 class HfMain(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
@@ -11,14 +12,39 @@ class HfMain(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         super(HfMain, self).__init__(parent)
         self.setupUi(self)
         self.folderButton.clicked.connect(self.addFolder)
-
-    @pyqtSlot()
-    def addFolder(self):
         self.dialog = QtWidgets.QDialog()
         self.dialog.ui = folderDialog.Ui_Dialog()
         self.dialog.ui.setupUi(self.dialog)
+        self.dialog.ui.folderOpenButton.clicked.connect(self.openFolder)
+        self.dialog.ui.buttonBox.accepted.connect(self.openFolderPost)
+
+    @pyqtSlot()
+    def addFolder(self):
         self.dialog.show()
         self.dialog.exec_()
+
+    @pyqtSlot()
+    def openFolder(self):
+        try:
+            open_dir = QtWidgets.QFileDialog.getExistingDirectory(
+                self,
+                "Open a folder",
+                os.path.expanduser("~"),
+                QtWidgets.QFileDialog.ShowDirsOnly)
+            with shelve.open('db') as db:
+                db['folderlist'].append(open_dir)
+                print(open_dir, db['folderlist'])
+                self.dialog.ui.folderEdit.setDuplicatesEnabled(False)
+                self.dialog.ui.folderEdit.insertItem(0, open_dir)
+                self.dialog.ui.folderEdit.setCurrentIndex(0)
+                db.close()
+        except ValueError:
+            print("Something went wrong")
+        self.dialog.raise_()  # put dialog on top
+
+    @pyqtSlot()
+    def openFolderPost(self):
+        pass
 
 
 def main():
